b46823ddac99c5d42151d7d8d9b615a409a5cc7d
limbospoon <magicspoon78@gmail.com>
2025-02-26 14:49:07 +0000

    game over retry loop working

--- assets/scripts/Utilities/scene_manager.gd
+++ assets/scripts/Utilities/scene_manager.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @onready var game_over_menu = %GameOverMenu
 
+signal _on_scene_loaded
+
 var house_scene_path = "res://scenes/house_scene.tscn"
 var create_a_dog_scene_path = "res://scenes/create-a-dog.tscn"
-var current_scene
+var current_scene: Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +21,8 @@ func goto_scene(path):
 	var scene_to_load = load(path).instantiate()
 	get_tree().root.add_child(scene_to_load)
 	current_scene = scene_to_load
+	
+	_on_scene_loaded.emit()
 
 func load_house_scene():
 	
@@ -31,23 +35,28 @@ func load_house_scene():
 	
 func load_create_a_dog():
 	print("Loading create-a-dog")
-	current_scene.queue_free()
+	
+	if current_scene != null:
+		current_scene.queue_free()
 	
 	await get_tree().create_timer(0.2).timeout
 	print("House scene unloaded")
 	
 	if not is_instance_valid(current_scene):
 		goto_scene(create_a_dog_scene_path)
-	
-func _on_load_createa_dog_button_up():
-	#load create-a-dog scene
-	goto_scene(create_a_dog_scene_path) 
-	
-	 #bind retry signal to load create-a-dog
-	game_over_menu.on_retry.connect(load_create_a_dog)
-	
+		
 	#bind create button to load house scene
 	var create_btn:Button = current_scene.get_node("%CreateDogButton")
 	create_btn.button_up.connect(load_house_scene)
+	
+func _on_load_createa_dog_button_up():
+	#load create-a-dog scene
+	load_create_a_dog()
+	
+	 #bind retry signal to load create-a-dog
+	game_over_menu.on_retry.connect(load_create_a_dog)
 
 	%"LoadCreate-a-Dog".hide()
+	
+func get_current_scene() -> Node2D:
+	return current_scene
--- assets/scripts/house_scene/game_over_menu.gd
+++ assets/scripts/house_scene/game_over_menu.gd
@@ -2,7 +2,9 @@ extends Control
 
 signal on_retry
 
-@onready var level_manager = get_node("%LevelManager")
+@onready var scene_manager = %SceneManager
+var level_manager
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
@@ -11,13 +13,21 @@ func _ready():
 func _process(delta):
 	pass
 
-func _on_level_manager_on_game_state_changed():
-	var game_manager_ref = level_manager
-	var current_state = game_manager_ref.get_game_state()
+func _on_game_state_changed():
+	var current_state = level_manager.get_game_state()
 	#check if game is over
-	if current_state == game_manager_ref.EGame_State.GAMEOVER:
+	if current_state == level_manager.EGame_State.GAMEOVER:
 		visible = true
 
 
 func _on_try_again_button_button_up():
 	on_retry.emit()
+	visible = false
+
+
+func _on_scene_manager__on_scene_loaded():
+	if level_manager == null:
+		level_manager = scene_manager.get_current_scene().get_node("%LevelManager")
+	
+	if level_manager != null:
+		level_manager.on_game_state_changed.connect(_on_game_state_changed)		
